3fc9127489bbc170cf5e0a483c73240c5c7b8cca
TP20 <thujan2@gmail.com>
2018-02-03 22:20:42 -0500

    Registration with firebase

--- app/src/main/java/test/coachscorner/MainActivity.java
+++ app/src/main/java/test/coachscorner/MainActivity.java
@@ -1,20 +1,107 @@
 package test.coachscorner;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 public class MainActivity extends CustomActionBarView {
+
+    private EditText emailInput;
+    private EditText passwordInput;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailInput = (EditText) findViewById(R.id.username_input);
+        passwordInput= (EditText) findViewById(R.id.password_input);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Sign In", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("Sign Out", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
     }
 
-    public void createAccount(View view) {
+    public void toRegistration(View view) {
         Intent i = new Intent(this, RegisterActivity.class);
         startActivity(i);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    public void onClickLogin(View view) {
+        String username = emailInput.getText().toString();
+        String password= passwordInput.getText().toString();
+
+        if (username.length()==0 || password.length() == 0) {
+            Toast.makeText(MainActivity.this, "Invalid Credentials",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Success", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if (task.isSuccessful()) {
+                            Intent i = new Intent(MainActivity.this, FirstPageActivity.class);
+//                            i.putExtra("Key", user.getEmail());
+                            startActivity(i);
+                        }
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        else {
+                            Log.w("Success", "signInWithEmail:failed", task.getException());
+                            Toast.makeText(MainActivity.this, "Invalid Credentials",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
 }
--- app/src/main/java/test/coachscorner/RegisterActivity.java
+++ app/src/main/java/test/coachscorner/RegisterActivity.java
@@ -1,13 +1,108 @@
 package test.coachscorner;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class RegisterActivity extends CustomActionBarView {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText emailInput;
+    private EditText passwordInput;
+    private EditText usernameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        emailInput = (EditText) findViewById(R.id.email_input);
+        passwordInput= (EditText) findViewById(R.id.password_input);
+       // usernameInput = (EditText) findViewById(R.id.username_input);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Signed in ", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("Signed out", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+        // [END auth_state_listener]
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.signOut();
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void createAccountNow(View view) {
+
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        if (password.length()<8 || email.equals("") || !email.contains("@")) {
+            Toast.makeText(this, "Please fill out all fields, password must also be at least 8 characters", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            Toast.makeText(RegisterActivity.this, " Done",
+                                    Toast.LENGTH_SHORT).show();
+                        //    updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                       //     updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                       // hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+ //[END create_user_with_email]
+
+
+
+
+    }
+
+
 }
